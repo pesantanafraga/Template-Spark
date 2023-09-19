09f25d22fd96148c69cba93216a62e1a89e097c4
pesantanafraga <pesantanafraga@gmail.com>
2023-09-19 15:27:10 -0300

    Realizando configuração inicial do e-mail, usando javascript para pegar os dados do formulário e enviar para o servidor, feito com node.js para enviar o e-mail para o destinatário correto

--- js/email.js
+++ js/email.js
@@ -1,49 +1,26 @@
-const express = require('express');
-const bodyParser = require('bodyParser');
-const nodemailer = require('nodemailer');
-
-const app = express();
-const porta = 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'contato.html');
-});
-
-app.post('/enviar-email', (req, res) => {
-    const { nome, email, assunto, mensagem } = req.body;
-})
-
-const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'pendrivedotecnico@gmail.com',
-        pass: 'ybdaanbkapgifqzm'
-    }
-});
-
-const mailOptions = {
-    from: email,
-    to: 'pendrivedotecnico@gmail.com',
-    subject: assunto,
-    text: `Nome: ${nome}\nEmail: ${email}\nAssunto: ${assunto}\nMensagem: ${mensagem}`
-};
-
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        console.error(error);
-        res.send('Erro ao enviar e-mail.');
-    }else {
-        console.log('E-mail enviado: ' + info.response);
-        res.send('E-mail enviado com sucesso.');
-    }
-});
-
-app.listen(porta, () => {
-    console.log(`Servidor rodando em http://localhost:${porta}`);
-});
+//Configuração para envio do e-mail
+document.getElementById('form-contato').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+    var assunto = document.getElementById('assunto').value;
+    var mensagem = document.getElementById('mensagem').value;
+  
+    fetch('/enviar-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ nome: nome, email: email, assunto: assunto, mensagem: mensagem }),
+    })
+    .then(response => response.text())
+    .then(data => alert(data))
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  });
+  
 
 
 //Validação de formulário de e-mail
---
+++ js/sendMail.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+let express = require('express');
+let nodemailer = require('nodemailer');
+let bodyParser = require('body-parser');
+
+let app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/enviar-email', function(req, res) {
+  let nome = req.body.nome;
+let email = req.body.email;
+let assunto = req.body.assunto;
+let mensagem = req.body.mensagem;
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+let mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: 'santanabistec@gmail.com',
+    subject: assunto,
+    text: 'Nome: ' + nome + '\nEmail: ' + email + '\nMensagem: ' + mensagem
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+      res.send('error');
+    } else {
+      console.log('Email enviado: ' + info.response);
+      res.send('sucesso');
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Aplicação rodando na porta 3000!');
+});
